fee5e13857a7ab175e1939b2a945cc51c5494432
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2023-02-10 20:16:38 -0300

    Update Script.js

--- Modulo E/Aula 16/Script.js
+++ Modulo E/Aula 16/Script.js
@@ -6,12 +6,16 @@ var res = document.querySelector('div#res')
 var c = 1
 var resultado = 0
 res.innerHTML = ''
-while (c <= 10){
-    resultado = num*c
-    res.innerHTML += `${num} X ${c} = ${resultado} <br>` 
-    c++
-}
 
+if (numf.value.length == 0){
+    window.alert('Por favor digite um numero')
+} else{
+    while (c <= 10){
+        resultado = num*c
+        res.innerHTML += `${num} X ${c} = ${resultado} <br>` 
+        c++
+    }
+    }
 
 //res.innerHTML = `teste ${num}`
 
