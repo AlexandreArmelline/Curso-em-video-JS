cfe5dcc3f06896f681249ef544b99db899f5f295
ALEXANDRE ARMELLINE DA SILVA <alexandrearmelline@gmail.com>
2023-02-23 21:17:52 -0300

    terminei
    Terminei o exercício do modulo F

--- Modulo F/Script.js
+++ Modulo F/Script.js
@@ -1,7 +1,6 @@
     var numf = document.querySelector('input#num')
-   
     var flist = document.querySelector('select#flist')
-    var res2 = document.querySelector('div#res2')
+    var res = document.querySelector('div#res')
     var valores = []
 
     function validarNum(n){
@@ -13,7 +12,7 @@
     }
 
     function inList(n, v){
-        if (v.indexOf(Number(n)) != -1){
+        if (v.indexOf(n) != -1){
             return true
         } else {
             return false
@@ -29,6 +28,7 @@
             var item = document.createElement('option')
             item.text = num
             flist.appendChild(item)
+            res.innerHTML = ''
 
         } else {
             window.alert('Valor invalido ou ja encontrado na lista')
@@ -37,6 +37,34 @@
         numf.value = ''
         numf.focus()
        
+    }
+
+    function final(){
+        if (valores.length == 0){
+            window.alert('Add pelo menos um numero')
+        } else {
+            var tot = valores.length
+            var maior = valores[0]
+            var menor = valores[0]
+            var soma = 0
+            var media = 0
+            for(var pos in valores){
+                soma += valores[pos]
+                if (valores[pos] > maior){
+                    maior = valores[pos]
+                }
+                if (valores[pos] < menor){
+                    menor = valores[pos]
+                }
+            }
+            media = soma/tot
+            res.innerHTML = ''
+            res.innerHTML += `<p>${tot} numeros cadastrados</p>`
+            res.innerHTML += `<p>O maior numero é ${maior}</p>`
+            res.innerHTML += `<p>O menor numero é ${menor}</p>`
+            res.innerHTML += `<p>A soma é ${soma}</p>`
+            res.innerHTML += `<p>A media é ${media}</p>`
+        }
     }
 
    
